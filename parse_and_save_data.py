7edf6e9ac5192b5562a09a675fb7168be9f20a74
sexozavr <daniildiveev@mail.ru>
2022-10-09 21:02:39 +0300

    [ADD] wrapped data parsing and saving into a separate script

---
+++ parse_and_save_data.py
@@ -0,0 +1,16 @@
+import json
+from parsers import *
+
+url = "https://www.kinopoisk.ru/lists/movies/country--1/?ss_subscription=ANY"
+
+movie_links = parse_links(url)
+descriptions, rates, genres = parse_data(movie_links, 2)
+
+data = {
+    'descriptions' : descriptions,
+    'rates' : rates, 
+    'genres' : genres
+}
+
+with open("descriptions_and_rates.json", "w") as f:
+    json.dump(data, f)
